a229f6441673fc223dbaabc958d15895aaa5ab2d
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-09-14 15:24:03 +0200

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ let count = 0;
 function click() {
   count++;
   button.innerHTML = "ClickMe: " + count;
+
+  setTimeout(function(){ alert("Nice score"); }, 5000);
 }
+
 
 button.onclick = click;
 button.innerHTML = "ClickMe: " + count;
+
